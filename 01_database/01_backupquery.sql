a156801b5e7e65382d9cc6a8940a0e564f5b05d7
Emilia Lozowska <lozowska.emilia@gmail.com>
2025-07-16 19:09:06 +0200

    csv with starts_before ETL

--- 01_database/01_backupquery.sql
+++ 01_database/01_backupquery.sql
@@ -1,2 +1,2 @@
 BACKUP DATABASE [Solar_System]
-TO DISK = 'D:\__EMI\SolarSystem_dashboard\01_database\SolarSystem_database';
+TO DISK = 'C:\EMI_\GitHub_repos\Solar_System\SolarSystem_dashboard\01_database\SolarSystem_database.bak';
